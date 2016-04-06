b19e158cac53bef50531fab0a4b6a53d62e18622
Mike Schennum and Mary Warrington <mikeschenphoto@gmail.com>
2016-04-06 13:43:29 -0700

    Add total price computation

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  cartTotal: Ember.computed("shoppingCart.items", function() {
+    var orderPrice = 0;
+    for (var i = 0; i < this.get('shoppingCart.items.length'); i++) {
+      orderPrice += this.get('shoppingCart.items')[i].get('price');
+    }
+    console.log(orderPrice);
+    return orderPrice;
+  })
 });
--- app/routes/view-cart.js
+++ app/routes/view-cart.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
+  cartTotal: Ember.computed("shoppingCart.items", function() {
+    return "test";
+    // var orderPrice = 0;
+    // for (var i = 0; i < this.get('shoppingCart.items.length'); i++) {
+    //   orderPrice += this.get('shoppingCart.items')[i].get('price');
+    // }
+    // console.log(orderPrice);
+    // return orderPrice;
+  })
 });
