fb737bffab466efcb66e7aea420be31186d2e02e
Mike Schennum and Mary Warrington <marywarrington@gmail.com>
2016-04-06 09:37:22 -0700

    Add category tile

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'shopping-cart',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://git-out-hub.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
