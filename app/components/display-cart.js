b1ba4374593d53bb76f74462ef3ef07aad2e0dd2
Mike Schennum and Mary Warrington <mikeschenphoto@gmail.com>
2016-04-06 11:39:30 -0700

    Fix shopping cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  shoppingCart: Ember.inject.service()
+  shoppingCart: Ember.inject.service(),
 });
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('store');
   this.route('category', {path: '/category/:category_id'});
+  this.route('view-cart');
 });
 
 export default Router;
