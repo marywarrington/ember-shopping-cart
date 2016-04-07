bb16ed624007c20056a4d450f113aac4cead251f
Mike Schennum and Mary Warrington <marywarrington@gmail.com>
2016-04-07 10:45:02 -0700

    adds time stamp to review

--- app/models/review.js
+++ app/models/review.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   reviewer: DS.attr(),
   review: DS.attr(),
-  product: DS.belongsTo('product', { async: true })
+  product: DS.belongsTo('product', { async: true }),
+  date: DS.attr('date', { defaultValue() { return new Date(); }})
 });
